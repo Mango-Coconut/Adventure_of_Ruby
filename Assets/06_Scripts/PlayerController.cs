3ea6d599a2374e9defc524f887d30b8924adcb1a
OhSeungSe <39735663+Mango-Coconut@users.noreply.github.com>
2025-09-02 15:45:40 +0900

    250902

--- Assets/06_Scripts/PlayerController.cs
+++ Assets/06_Scripts/PlayerController.cs
@@ -1,38 +1,20 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {   
     [SerializeField] float moveSpeed = 5;
-    public InputAction LeftAction;
-    public InputAction RightAction;
+    public InputAction MoveAction;
     void Start()
     {
-        LeftAction.Enable();
+        MoveAction.Enable();
     }
 
     void Update()
     {
-        float h = 0;
-        float v = 0;
-        if (LeftAction.IsPressed())
-        {
-            h = -1;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            h = 1;
-        }
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            v = 1;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            v = -1;
-        }
-        Vector3 position = transform.position;
-        Vector3 newv = new Vector3(h, v, 0).normalized * moveSpeed * Time.deltaTime;
-        transform.position += newv;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + move * Time.deltaTime;
+        transform.position = position;
     }
 }
